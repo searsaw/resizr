68cf2d7daff60255c6b568e4ce40082fb01fdedb
Alex Sears <me@alexsears.com>
2017-02-08 23:57:46 -0500

    Able to receive a filename and resize it

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
+const { resolve } = require('path');
+const sharp = require('sharp');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
+const IMAGES_DIR = resolve(__dirname, '..', 'images');
 
 const app = express();
 
 app.use(morgan(NODE_ENV === 'production' ? 'combined' : 'dev'));
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-})
+app.get('/:file', (req, res) => {
+  const { file } = req.params;
+  const [filename, ext] = file.split('.');
+
+  sharp(`${IMAGES_DIR}/${file}`)
+    .resize(100, 100)
+    .max()
+    .toFile(`${IMAGES_DIR}/${filename}_smaller.${ext}`)
+    .then(() => {
+      res.send('Done mothafucka');
+    });
+});
 
 module.exports = app;
