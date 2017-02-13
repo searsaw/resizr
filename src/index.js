8298e39cec13137ffdbc26496eb319c30dc09c91
Alex Sears <me@alexsears.com>
2017-02-12 23:54:17 -0500

    Able to submit image from webpage and download to browser, all streamed

--- src/index.js
+++ src/index.js
@@ -1,26 +1,55 @@
+const { resolve } = require('path');
+const archiver = require('archiver');
+const bodyParser = require('body-parser');
+const Busboy = require('busboy');
+const debug = require('debug')('app:index');
 const express = require('express');
 const morgan = require('morgan');
-const { resolve } = require('path');
 const sharp = require('sharp');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
-const IMAGES_DIR = resolve(__dirname, '..', 'images');
+const ROOT_DIR = resolve(__dirname, '..');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan(NODE_ENV === 'production' ? 'combined' : 'dev'));
+app.set('view engine', 'pug');
+app.set('views', resolve(ROOT_DIR, 'templates'));
 
-app.get('/:file', (req, res) => {
-  const { file } = req.params;
-  const [filename, ext] = file.split('.');
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-  sharp(`${IMAGES_DIR}/${file}`)
-    .resize(100, 100)
-    .max()
-    .toFile(`${IMAGES_DIR}/${filename}_smaller.${ext}`)
-    .then(() => {
-      res.send('Done mothafucka');
-    });
+app.post('/', (req, res) => {
+  debug('Posting the image');
+
+  const bb = new Busboy({ headers: req.headers });
+  const pipeline = sharp().max();
+  const archive = archiver('zip', { zlib: { compression: 9 } });
+
+  bb.on('file', (fieldname, file, filename) => {
+    debug('Processing file', filename);
+
+    const [name, ext] = filename.split('.');
+
+    for (let i = 550; i >= 200; i--) { // eslint-disable-line no-plusplus
+      archive.append(pipeline.clone().resize(i, i), { name: `${name}_${i}x${i}.${ext}` });
+    }
+
+    archive.finalize();
+
+    file.pipe(pipeline);
+  });
+
+  const now = new Date();
+  const [day, time] = now.toISOString().split('T');
+  const archiveName = `${day}-${time}_images.zip`;
+
+  res.attachment(archiveName);
+  req.pipe(bb);
+  archive.pipe(res);
 });
 
 module.exports = app;
